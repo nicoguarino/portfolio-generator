ddcf6a8b1773c9f91560956e14096e3e57f67851
Nico (Filipu) Guarino <nizoworld@gmail.com>
2021-09-19 12:47:26 -0700

    added git.ignore, added package.json, package-lock.json, pulled inquire npm, made sure inquire was linked properly

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+const { type } = require('os');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-fs.writeFile('index.html' , generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// const pageHTML = generatePage(name, github);
 
-    console.log('Portfolio complete! Check out index.html to see the ouput!');
-});
+// fs.writeFile('./index.html' , pageHTML, err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the ouput!');
+// });
+
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers));
