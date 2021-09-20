b3d1b325cbb34b9d5b46fd3adf569fe35fe4e561
Nico (Filipu) Guarino <nizoworld@gmail.com>
2021-09-20 15:03:15 -0700

    added generateprojects function

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const { type } = require('os');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 // const pageHTML = generatePage(name, github);
 
@@ -144,6 +144,13 @@ if (!portfolioData.projects){
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    console.log(portfolioData);
+
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+        if (err) throw new Error(err);
+
+        console.log('Page created! Check out index.html in this directory to see it!');
+    });
 });
 
